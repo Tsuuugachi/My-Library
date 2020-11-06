aa0f39ec7ba2879b0f14972d53e48eac4b43800d
shiro <1801201@s.asojuku.ac.jp>
2020-11-06 10:50:38 +0900

    ユーザー画面　編集

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //SearchView追加
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubar, menu);
+
+        return true;
+    }
+
 }
