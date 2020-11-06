eb0c790c7dbf482d4df0c17109a80d7a82b67cda
magari <1801185@s.asojuku.ac.jp>
2020-11-06 11:56:20 +0900

    #101Login画面　編集

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -13,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(オプションメニューのxmlファイル名, menu);
+        return true;
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuber, menu);
+
+        return true;
+    }
 }
