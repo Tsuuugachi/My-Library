4f41542ba383dedb3dbae6cd2daf8fa691b819c5
shiro <1801201@s.asojuku.ac.jp>
2020-11-13 10:16:55 +0900

    #120 User_Read_ListDetails

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.user_read_listdetails);
     }
 }
 
