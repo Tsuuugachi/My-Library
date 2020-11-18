25706e7b058c2609295c797e153eae5dac8b3a1e
oshita <1801151@s.asojuku.ac.jp>
2020-11-18 14:12:19 +0900

    ひとまずログイン

--- app/src/main/java/com/example/my_library/UserAuthActivity.java
+++ app/src/main/java/com/example/my_library/UserAuthActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,36 +15,73 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class MainActivity extends AppCompatActivity {
+public class UserAuthActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "UserAuthActivity";
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String Image = "user_image";
+    private static final String Pass = "user_password";
+    private static final String Name = "user_name";
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tester_main);
 
         mAuth = FirebaseAuth.getInstance();
-//新規登録ボタン
+        //新規登録ボタン
         findViewById(R.id.create_user_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = ((TextView)findViewById(R.id.email_txt)).getText().toString();
-                String password = ((TextView)findViewById(R.id.password_txt)).getText().toString();
-                createUser(email, password);
+                String user_password = ((TextView)findViewById(R.id.password_txt)).getText().toString();
+                String user_name = ((TextView)findViewById(R.id.name_txt)).getText().toString();
+                String user_image = "image.jpeg";
+
+                Map<String, Object> note =new HashMap<>();
+                note.put(Image, user_image);
+                note.put(Name, user_name);
+                note.put(Pass, user_password);
+
+                db.collection("user").document(email).set(note)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(UserAuthActivity.this, "NoteSaved", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener(){
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(UserAuthActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, e.toString());
+                            }
+                        });
+
+                createUser(email, user_password);
+
             }
         });
+
         //ログインボタン
         findViewById(R.id.sign_in_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,22 +99,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void buildActionCodeSettings() {
-        // [START auth_build_action_code_settings]
-        ActionCodeSettings actionCodeSettings =
-                ActionCodeSettings.newBuilder()
-                        // URL you want to redirect back to. The domain (www.example.com) for this
-                        // URL must be whitelisted in the Firebase Console.
-                        .setUrl("https://mylibrary01.page.link")
-                        // This must be true
-                        .setHandleCodeInApp(true)
-                        .setIOSBundleId("com.example.ios")
-                        .setAndroidPackageName(
-                                "com.example.android",
-                                true, /* installIfNotAvailable */
-                                "12"    /* minimumVersion */)
-                        .build();
-        // [END auth_build_action_code_settings]
+  /*  public void sendEmailVerificationWithContinueUrl() {
+        // [START send_email_verification_with_continue_url]
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        String url = "https://mylibrary01.page.link" + user.getUid();
+        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+                .setUrl(url)
+                .setIOSBundleId("com.example.ios")
+                // The default for this is populated with the current android package name.
+                .setAndroidPackageName("com.example.android", false, null)
+                .build();
+
+        user.sendEmailVerification(actionCodeSettings)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
     }
 
     public void sendSignInLink(String email, ActionCodeSettings actionCodeSettings) {
@@ -126,7 +170,8 @@ public class MainActivity extends AppCompatActivity {
         }
         // [END auth_verify_sign_in_link]
     }
-//新規登録処理
+    */
+    //新規登録処理
     private void createUser(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
