fd9927750943f72df0422563b38f65d11ba1e124
magari <1801185@s.asojuku.ac.jp>
2020-11-27 15:06:28 +0900

    #204　mypage_layout　変更

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.mypage);
     }
 
     //ヘッダー　メニュー表示
