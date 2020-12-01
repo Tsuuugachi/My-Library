8e8110a15edd13ffae6993edf17897416b7be2c9
shiro <1801201@s.asojuku.ac.jp>
2020-12-01 16:09:15 +0900

    #123 Favorite_Book

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -4,24 +4,273 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.view.Gravity;
 
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.Menu;
 import android.view.ViewGroup;
-import android.view.View;
 import android.widget.EditText;
-import android.os.Bundle;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite_book);
+
+
+        //　コンテキストメニュー表示
+        View view1 = findViewById(R.id.imageView1);
+        View view2 = findViewById(R.id.imageView2);
+        View view3 = findViewById(R.id.imageView3);
+        View view4 = findViewById(R.id.imageView4);
+        View view5 = findViewById(R.id.imageView5);
+        View view6 = findViewById(R.id.imageView6);
+        View view7 = findViewById(R.id.imageView7);
+        View view8 = findViewById(R.id.imageView8);
+        View view9 = findViewById(R.id.imageView9);
+
+        view1.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView1選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view2.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView2選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view3.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView3選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view4.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView4選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view5.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView5選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view6.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView6選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view7.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView7選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view8.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView8選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
+        view9.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                getMenuInflater().inflate(R.menu.context_menu, contextMenu);
+
+                //　imageView9選択時の処理を追加
+                contextMenu.findItem(R.id.book_Details).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                contextMenu.findItem(R.id.book_Delete).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast toast = Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+
     }
 
 
-    //ヘッダー　メニュー表示
+    //　ヘッダーメニュー表示
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -29,8 +278,5 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
-
-
-
 
 }
