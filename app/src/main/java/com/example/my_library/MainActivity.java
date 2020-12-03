3e48f1b736b3d067df512845ec4d3f404826506c
shiro <1801201@s.asojuku.ac.jp>
2020-12-03 15:54:41 +0900

    #127 Withdrawal_ Done
    ログイン画面へ遷移するボタンを追加しました。

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logout_verification);
+        setContentView(R.layout.withdrawal_done);
     }
 
 
