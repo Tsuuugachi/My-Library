e2910735bdb39c0fb8ce359ad4ab75af94b87f03
shiro <1801201@s.asojuku.ac.jp>
2020-12-04 17:25:49 +0900

    #118 User_FollowList

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
