d434d9aec2fd5c93856daa5ebdbba16f7fc9e00a
shiro <1801201@s.asojuku.ac.jp>
2020-12-04 17:22:35 +0900

    #118 User_FollowList

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -19,35 +19,6 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * アクティビティ生成時に呼ばれる
-     * @param savedInstanceState
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-            setContentView(R.layout.user_followlist_frame);
-
-            // レイアウトからリストビューを取得
-            ListView listView = (ListView)findViewById(R.id.account);
-
-            // リストビューに表示する要素を設定
-            ArrayList<SampleListItem> listItems = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.id.imageView);
-                SampleListItem item = new SampleListItem(bmp, "name" + String.valueOf(i));
-                listItems.add(item);
-            }
-
-            // 出力結果をリストビューに表示
-            SampleListAdapter adapter = new SampleListAdapter(this, R.layout.user_followlist, listItems);
-            listView.setAdapter(adapter);
-
-    }
-
-
-
     //ヘッダー　メニュー表示
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
