433bb20546ef9c6344994db723b32dd9f09c93b3
shiro <1801201@s.asojuku.ac.jp>
2020-12-04 17:26:29 +0900

    #118 User_FollowList

--- app/src/main/java/com/example/my_library/MainActivity.java
+++ app/src/main/java/com/example/my_library/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.user_followlist_frame);
     }
 
 
