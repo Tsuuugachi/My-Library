c84af019a0f8817a342491ad89ac5ba30dfaeec0
Tsuuugachi <1801169@s.asojuku.ac.jp>
2020-12-11 15:01:32 +0900

    リードリスト一覧画面のソースコード

--- app/src/main/java/com/example/my_library/activity/UserDeleteActivity.java
+++ app/src/main/java/com/example/my_library/activity/UserDeleteActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.my_library.R;
+import com.example.my_library.bean.DataBean;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
